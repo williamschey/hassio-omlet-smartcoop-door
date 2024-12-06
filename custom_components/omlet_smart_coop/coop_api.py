a1e885f67c047d15b97b9778b473dd006a6011cc
William Schey <schey_william@cat.com>
2024-12-07 09:19:39 +1000

    Make Polling Work

--- custom_components/omlet_smart_coop/coop_api.py
+++ custom_components/omlet_smart_coop/coop_api.py
@@ -1,20 +1,35 @@
 from smartcoop.client import SmartCoopClient
 from smartcoop.api.omlet import Omlet
+import datetime
 
 class SmartCoopAPI:
     def __init__(self, api_key):
         client = SmartCoopClient(client_secret=api_key)
         self.omlet = Omlet(client)
+        self.devices = self.omlet.get_devices()
+        self.last_update = datetime.datetime.now()
 
     def get_devices(self):
         """Retrieve all devices from the API."""
-        return self.omlet.get_devices()
+        self.refresh(self)
+        return self.devices
+    
+    def refresh(self):
+        newNow = datetime.datetime.now()
+        if abs((self.last_update - newNow).seconds) > 10:
+            self.devices = self.omlet.get_devices()
+            self.last_update = datetime.datetime.now()
+
 
     def get_device_state(self, device, key):
         """Get a specific state of a device."""
-        return getattr(device.state, key)
+        self.refresh(self)
+        mydevice = next((updateddevice for updateddevice in self.devices if updateddevice.deviceId == device.deviceId), None)
+        return getattr(mydevice.state, key)
 
     def perform_action(self, device, key, value):
         """Set a specific state for a device."""
-        open_action = next((action for action in device.actions if action.name == key), None)
-        self.omlet.perform_action(open_action)
+        mydevice = next((updateddevice for updateddevice in self.devices if updateddevice.deviceId == device.deviceId), None)
+        omlet_action = next((action for action in mydevice.actions if action.name == key), None)
+        self.hass.async_add_executor_job(self.omlet.perform_action, omlet_action)    
+        
